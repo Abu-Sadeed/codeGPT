5e081bcfd70b6c311710f3df5873b2a3940dae95
Sadeed <abusadeed@iut-dhaka.edu>
2022-12-24 19:29:14 +0600

    Created the html skeleton

---
+++ client/script.js
@@ -0,0 +1,5 @@
+import bot from "./assets/assets/bot.svg";
+import user from "./assets/assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
